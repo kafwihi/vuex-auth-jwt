b1214bfa4dfb0b6d033134f5e7ee073b1391f24c
kafwihi <kafwihi@gmail.com>
2019-10-26 14:02:09 +0300

    a project for authentication of user access,uses sqlite database

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,34 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
-</template>
+ <template>
+      <div id="app">
+        <div id="nav">
+          <router-link to="/">Home</router-link> |
+          <router-link to="/about">About</router-link>
+          <span v-if="isLoggedIn"> | <a @click="logout">Logout</a></span>
+          <span v-else> | <router-link to="/login">Login</router-link></span>
+        </div>
+        <router-view/>
+      </div>
+    </template>
+ <script>
+    export default {
+      computed: {
+        isLoggedIn: function() {
+          return this.$store.getters.isLoggedIn;
+        }
+      },
+      methods: {
+        logout: function() {
+          this.$store.dispatch("logout").then(() => {
+            this.$router.push("/login");
+          });
+        }
+      }
+    };
+    </script>
 
 <style>
+
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
